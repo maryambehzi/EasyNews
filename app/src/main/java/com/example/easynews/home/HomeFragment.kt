7a25d3fa14fed7996843705b5604a7bf802250ef
maryambehzi <mary.behzi@gmail.com>
2022-06-15 14:41:30 +0430

    first commit

---
+++ app/src/main/java/com/example/easynews/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.easynews.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.easynews.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = FragmentHomeBinding.inflate(inflater)
+
+        return binding.root
+    }
+}
